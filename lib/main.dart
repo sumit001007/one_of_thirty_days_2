55aa0acd882ea15a7907f53527c18b11b253da82
sumit001007 <sumitapp22@gmail.com>
2021-08-29 22:34:20 +0530

    after transform

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,31 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'App 1',
       home: Scaffold(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.grey,
         appBar: AppBar(
           title: Text('Day 1'),
           centerTitle: false,
           leading: Icon(Icons.home),
           backgroundColor: Colors.green,
         ),
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ) 
-       ,),
+        body: Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.center,
+              child: Text('Hey Coders !!!'),
+              decoration: BoxDecoration(
+                border: Border.all(width: 4,color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.orangeAccent,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.orange, offset: Offset(6.0,6.0),
+                  )
+                ]
+              ),
+              transform: Matrix4.rotationZ(0.1),
+        ),
       ),
     );
   }
